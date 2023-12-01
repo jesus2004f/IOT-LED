701e4d2ad27b0f4aed8e3fb6f3cf94d866896ff5
YISUS <122810458+jesus2004f@users.noreply.github.com>
2023-12-01 16:03:01 +0000

    se actualizo

---
+++ main.py
@@ -0,0 +1,72 @@
+import fastapi
+import sqlite3
+from pydantic import BaseModel
+
+
+# Conecta a la base de datos
+conn = sqlite3.connect("sql/dispositivos.db")
+
+app = fastapi.FastAPI()
+
+origins = [
+    "http://127.0.0.1:8000",
+    "http://localhost:8000",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class Dispositivos(BaseModel):
+    id_dispositivo: int
+    dispositivo: str
+    valor: int
+
+@app.post("/dispositivos")
+async def crear_dispositivos(dispositivo: Dispositivos):
+    c = conn.cursor()
+    c.execute('INSERT INTO dispositivos (id_dispositivo, dispositivo, valor) VALUES (?, ?, ?)',
+              (dispositivo.id_dispositivo, dispositivo.dispositivo, dispositivo.valor))
+    conn.commit()
+    return dispositivo
+
+@app.get("/dispositivos")
+async def obtener_dispositivos():
+    """Obtiene todos los dispositivos."""
+    c = conn.cursor()
+    c.execute('SELECT * FROM dispositivos;')
+    response = []
+    for row in c:
+        dispositivo = {"id_dispositivo": row[0], "dispositivo": row[1], "valor": row[2]}
+        response.append(dispositivo)
+    return response
+
+@app.get("/dispositivos/{id_dispositivo}")
+async def obtener_valor_dispositivo(id_dispositivo: int):
+    """Obtiene el valor de un dispositivo por su id_dispositivo."""
+    c = conn.cursor()
+    c.execute('SELECT valor FROM dispositivos WHERE id_dispositivo = ?', (id_dispositivo,))
+    valor = c.fetchone()  # Obtiene la primera fila de la consulta
+    
+    return {"valor": valor[0] if valor else None}  
+
+
+@app.put("/dispositivos/{id_dispositivo}/{valor}")
+async def actualizar_dispositivo(id_dispositivo: int, valor: int):
+    """Actualiza un dispositivo."""
+    # Actualiza el dispositivo en la base de datos con el nuevo valor
+    c = conn.cursor()
+    c.execute('UPDATE dispositivos SET valor = ? WHERE id_dispositivo = ?', (valor, id_dispositivo))
+    conn.commit()
+
+    # Recupera el valor actualizado desde la base de datos
+    c.execute('SELECT valor FROM dispositivos WHERE id_dispositivo = ?', (id_dispositivo,))
+    nuevo_valor = c.fetchone()[0]
+
+    return {"message": "Dispositivo actualizado correctamente", "nuevo_valor": nuevo_valor}
+
